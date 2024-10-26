54f2bb7bb9d0c78c22cd8b3a55bf1b660521a3ac
duck <gracjan.chamulati2019@gmail.com>
2024-10-26 12:44:16 +0200

    update

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-from model import Uczen,Nauczyciel
+from model import Uczen,Nauczyciel,Klasa,nauczyciel_klasa
 from extensions import db 
 
 app = Flask(__name__)
@@ -17,11 +17,15 @@ with app.app_context():
 def home():
     if 'logged_in' in session:
         print("Witaj w domu!")
+        nauczyciel = Nauczyciel.query.filter_by(id=session["id"]).first()
+
+        klasy_nauczyciela = nauczyciel.klasy
+        print(klasy_nauczyciela)
     #tworzenie nowego usera
-    #new_user = Nauczyciel(imie="N", nazwisko="Drałus", login="qwerty",haslo="qwerty")
+    #new_user = Nauczyciel(imie="Jan", nazwisko="Kowalski", login="qwerty",haslo="qwerty")
     #db.session.add(new_user)
     #db.session.commit()
-    return render_template("main.html")
+    return render_template("main.html",klasy=klasy_nauczyciela)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -30,7 +34,12 @@ def login():
         login = request.form['login']
         haslo = request.form['haslo']
         nauczyciel = Nauczyciel.query.filter_by(login=login, haslo=haslo).first()
+
+       
         if nauczyciel:
+            session["id"] = nauczyciel.id
+            session["imie"] = nauczyciel.imie
+            session["nazwisko"] = nauczyciel.nazwisko
             session['logged_in'] = True
             return redirect(url_for('home'))
         else:
@@ -38,4 +47,29 @@ def login():
         
     return render_template("login.html")
     
+@app.route("/add_class", methods=[ 'POST'])
+def add_class():
+    if request.method == "POST":
+        klasa_nazwa = request.form['class']
+        print(klasa_nazwa)
+
+        nauczyciel_id = session['id']
+        nauczyciel = Nauczyciel.query.get(nauczyciel_id)
+
+        klasa = Klasa.query.filter_by(nazwa=klasa_nazwa).first()
+
+        if not klasa:
+            klasa = Klasa(nazwa=klasa_nazwa)
+            db.session.add(klasa)  #
+
+        if klasa not in nauczyciel.klasy:
+            nauczyciel.klasy.append(klasa)  
+
+        db.session.commit()
+
+        return redirect(url_for('home'))  
+
+        return redirect(url_for('home'))
+
+
 
--- model.py
+++ model.py
@@ -10,14 +10,30 @@ class Uczen(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+        
+nauczyciel_klasa = db.Table('nauczyciel_klasa',
+    db.Column('nauczyciel_id', db.Integer, db.ForeignKey('nauczyciele.id'), primary_key=True),
+    db.Column('klasa_id', db.Integer, db.ForeignKey('klasy.id'), primary_key=True)
+)
 
 class Nauczyciel(db.Model):
-    __tablename__ = 'Nauczyciel'
+    __tablename__ = 'nauczyciele'
     id = db.Column(db.Integer, primary_key=True)
-    imie = db.Column(db.String(80), unique=False, nullable=False)
-    nazwisko = db.Column(db.String(80), unique=False, nullable=False)
-    login = db.Column(db.String(20), unique=False, nullable=False)
-    haslo = db.Column(db.String(20), unique=False, nullable=False)
+    imie = db.Column(db.String(80), nullable=False)
+    nazwisko = db.Column(db.String(80), nullable=False)
+    login = db.Column(db.String(20), unique=True, nullable=False)
+    haslo = db.Column(db.String(20), nullable=False)
+
+    klasy = db.relationship('Klasa', secondary='nauczyciel_klasa', backref='nauczyciele')
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<Nauczyciel {self.imie} {self.nazwisko}>'
+
+
+class Klasa(db.Model):
+    __tablename__ = 'klasy'
+    id = db.Column(db.Integer, primary_key=True)
+    nazwa = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f'<Klasa {self.nazwa}>'
